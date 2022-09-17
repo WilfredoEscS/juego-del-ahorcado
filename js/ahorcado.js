ddef7743cf81a9b86a73d601a5edc0d84972e33a
Wilfredo Escobar <wilfredosorto75@gmail.com>
2022-09-16 20:57:35 -0600

    Formatea el código de manera más consistente

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,6 +1,7 @@
 //Iniciar juego
 function iniciarJuego() {
-    document.getElementById("pantalla-botones").style.display = "none";
-    document.getElementById("pantalla-juego").style.display = "grid";
-    document.getElementById("cuerpo").style.gridTemplateAreas = '"encabezado" "canvas" "pie-pagina"';
+  document.getElementById("pantalla-botones").style.display = "none";
+  document.getElementById("pantalla-juego").style.display = "grid";
+  document.getElementById("cuerpo").style.gridTemplateAreas =
+    '"encabezado" "canvas" "pie-pagina"';
 }
