241a023afdf61aad987b3130d650e642b7099cff
Wilfredo Escobar <wilfredosorto75@gmail.com>
2022-09-18 09:36:03 -0600

    Crea una función para elegir la palabra secreta

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,3 +1,22 @@
+//Selectores
+let listaDePalabras = [
+  "HTML",
+  "JAVASCRIPT",
+  "ALURA",
+  "ORACLE",
+  "EL SALVADOR",
+  "LATINOAMERICA",
+];
+let tablero = document.getElementById("ahorcado").getContext("2d");
+let palabraSecreta = "";
+
+//Palabra secreta
+function seleccionarPalabraSecreta() {
+  let palabra =
+    listaDePalabras[Math.floor(Math.random() * listaDePalabras.length)];
+  palabraSecreta = palabra;
+}
+
 //Iniciar juego
 function iniciarJuego() {
   document.getElementById("pantalla-botones").style.display = "none";
